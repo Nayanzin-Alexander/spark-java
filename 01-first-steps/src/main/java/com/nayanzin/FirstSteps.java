7dd778f2af285ba86a05c780862e291d6f153193
Oleksandr_Nayanzin <Oleksandr_Nayanzin@epam.com>
2019-10-23 10:15:22 +0300

    Gradle project created

---
+++ 01-first-steps/src/main/java/com/nayanzin/FirstSteps.java
@@ -0,0 +1,9 @@
+package com.nayanzin;
+
+import static com.nayanzin.sparkjava.common.Common.greet;
+
+public class FirstSteps {
+    public static void main(String[] args) {
+        System.out.println(greet("World"));
+    }
+}
---
+++ common/src/main/java/com/nayanzin/sparkjava/common/Common.java
@@ -0,0 +1,7 @@
+package com.nayanzin.sparkjava.common;
+
+public class Common {
+    public static String greet(String name) {
+        return "Hello, " + name + "!!!";
+    }
+}
